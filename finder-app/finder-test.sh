cdc6849072ff699b06537db5619bdb8dd8da13b9
temp <temp@Ubuntu.myguest.virtualbox.org>
2025-02-08 21:40:34 -0700

    Step-4 -> Modified file PATH in .txt file

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)  # Updated path
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=$(cat /etc/finder-app/conf/assignment.txt)  # Updated path
 
 if [ $assignment != 'assignment1' ]
 then
@@ -58,7 +58,12 @@ do
 	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# Run finder and write output to /tmp/assignment4-result.txt
+finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt
+
+#OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# Read the output from the file
+OUTPUTSTRING=$(cat /tmp/assignment4-result.txt)
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
