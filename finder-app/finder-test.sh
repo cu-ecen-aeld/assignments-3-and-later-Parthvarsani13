ce384cdb35ef4747239627bfdb46760969bc472e
Parth Varsani <parth.varsani@colorado.edu>
2025-02-09 19:33:17 -0700

    Update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,8 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat /etc/finder-app/conf/username.txt)  # Updated path
-
+#username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
@@ -32,8 +32,8 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat /etc/finder-app/conf/assignment.txt)  # Updated path
-
+#assignment=`cat ../conf/assignment.txt`
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 if [ $assignment != 'assignment1' ]
 then
 	mkdir -p "$WRITEDIR"
@@ -49,19 +49,24 @@ then
 	fi
 fi
 
+# Build the writer utility if it doesn't exist
+#if [ ! -f ./writer ]; then
+#	echo "Build writer Application "
+#	make clean
+#	make
+#fi
+
 #echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
-
+# replace writer with writer.sh
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
-
-# Write output to /tmp/assignment4-result.txt
-echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
